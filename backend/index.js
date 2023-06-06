ab4b035e184e9b3424fdd8f7f075db2d4653a36d
Chirag Bansal <chirag1525.be20@chitkara.edu.in>
2023-06-06 14:24:31 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const connectDB = require('./db');
 require('dotenv').config();
+const path = require('path')
 const port = process.env.PORT;
 
 const corsOptions = {
@@ -16,6 +17,11 @@ connectDB();
 app.use('/api/auth', require('./routes/auth'));
 app.get('/',(req,res)=>{
   res.send("Hello from server")
+})
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
 })
 app.listen(port, () => {
   console.log(`Server started on: http://localhost:${port}`);
